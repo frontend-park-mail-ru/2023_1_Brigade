b69850ef67e8f291c200c3fe9d8561bc9a82b558
Zela2520 <sugarbeatzela@gmail.com>
2023-03-18 19:02:43 +0300

    init own redux

--- src/features/chat/view/view.ts
+++ src/features/chat/view/view.ts
@@ -1,4 +1,10 @@
 class ViewChat {
+    // #elem
+
+    // constructor(...props, state) {
+    //     this.#elem = 
+    // };
+
     render(id = null as any) {
         if (!document.body.querySelector('.js-header')) {
             console.log('render function has been called');
