2255786ad3605090fde148d52886f7d8e0c6d0b5
marcussss1 <danilalakalash60@gmail.com>
2023-04-05 23:38:46 +0300

    flex writing

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,3 @@
-// import '@/index.css';
-// import { store } from '@store/store';
-// import {createAuthAction} from "@actions/authActions";
-//
-// store.dispatch(createAuthAction());
-
-// avatar
-//     :
-//     "https://oir.mobi/uploads/posts/2021-04/1619691521_16-oir_mobi-p-bolshaya-obezyana-zhivotnie-krasivo-foto-17.jpg"
-// email
-//     :
-//     "marcussss1@gmail.com"
-// id
-//     :
-//     1
-// nickname
-//     :
-//     "marcussss1"
-// status
-//     :
-//     "marcussss1"
-// username
-//     :
-//     "marcussss1"
-
-
-
 import '@/index.css';
 import { DumbContacts } from "@components/contacts/contacts"
 import { SmartContacts } from "@containers/contacts/contacts";
@@ -37,81 +10,6 @@ import {smallEllipseIconUI} from "@components/ui/small-ellipse-icon/small-ellips
 import {SmartCreateGroup} from "@containers/create_group/create_group";
 import {store} from "@store/store";
 
-// const createGroup = new DumbCreateGroup({
-//     ...
-//     [
-//         {
-//             avatar: "https://oir.mobi/uploads/posts/2021-04/1619691521_16-oir_mobi-p-bolshaya-obezyana-zhivotnie-krasivo-foto-17.jpg",
-//             email:  "marcussss1@gmail.com",
-//             id: 1,
-//             nickname: "marcussss1",
-//             status: "marcussss1",
-//             username: "marcussss1",
-//         },
-//         {
-//             avatar: "https://oir.mobi/uploads/posts/2021-04/1619691521_16-oir_mobi-p-bolshaya-obezyana-zhivotnie-krasivo-foto-17.jpg",
-//                 email:  "marcussss1@gmail.com",
-//             id: 1,
-//             nickname: "marcussss1",
-//             status: "marcussss1",
-//             username: "marcussss1"
-//         },
-//         {
-//             avatar: "https://oir.mobi/uploads/posts/2021-04/1619691521_16-oir_mobi-p-bolshaya-obezyana-zhivotnie-krasivo-foto-17.jpg",
-//             email:  "marcussss1@gmail.com",
-//             id: 1,
-//             nickname: "marcussss1",
-//             status: "marcussss1",
-//             username: "marcussss1"
-//         },
-//         {
-//             avatar: "https://oir.mobi/uploads/posts/2021-04/1619691521_16-oir_mobi-p-bolshaya-obezyana-zhivotnie-krasivo-foto-17.jpg",
-//             email:  "marcussss1@gmail.com",
-//             id: 1,
-//             nickname: "marcussss1",
-//             status: "marcussss1",
-//             username: "marcussss1"
-//         },
-//     ]
-// })
-
-// const crtGroup = new SmartCreateGroup({...
-//             [
-//                 {
-//                     avatar: "https://oir.mobi/uploads/posts/2021-04/1619691521_16-oir_mobi-p-bolshaya-obezyana-zhivotnie-krasivo-foto-17.jpg",
-//                     email:  "marcussss1@gmail.com",
-//                     id: 1,
-//                     nickname: "marcussss1",
-//                     status: "marcussss1",
-//                     username: "marcussss1",
-//                 },
-//                 {
-//                     avatar: "https://oir.mobi/uploads/posts/2021-04/1619691521_16-oir_mobi-p-bolshaya-obezyana-zhivotnie-krasivo-foto-17.jpg",
-//                     email:  "marcussss1@gmail.com",
-//                     id: 1,
-//                     nickname: "marcussss1",
-//                     status: "marcussss1",
-//                     username: "marcussss1"
-//                 },
-//                 {
-//                     avatar: "https://oir.mobi/uploads/posts/2021-04/1619691521_16-oir_mobi-p-bolshaya-obezyana-zhivotnie-krasivo-foto-17.jpg",
-//                     email:  "marcussss1@gmail.com",
-//                     id: 1,
-//                     nickname: "marcussss1",
-//                     status: "marcussss1",
-//                     username: "marcussss1"
-//                 },
-//                 {
-//                     avatar: "https://oir.mobi/uploads/posts/2021-04/1619691521_16-oir_mobi-p-bolshaya-obezyana-zhivotnie-krasivo-foto-17.jpg",
-//                     email:  "marcussss1@gmail.com",
-//                     id: 1,
-//                     nickname: "marcussss1",
-//                     status: "marcussss1",
-//                     username: "marcussss1"
-//                 },
-//             ]
-//         })
-// const createGroup = new
 const crtGroup = new SmartCreateGroup({
     ...
         [
@@ -144,5 +42,3 @@ const crtGroup = new SmartCreateGroup({
 });
 
 crtGroup.render();
-
-// document.querySelector('#root')?.innerHTML = crtGroup.render()
