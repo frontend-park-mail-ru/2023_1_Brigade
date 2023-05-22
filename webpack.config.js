03f361ef701031a773a2169862d5d8caf84f97f4
Gevork Gabrielyan <gevorkgabrielan@gmail.com>
2023-05-22 15:22:06 +0300

    Added manifest and icons in assets for pwa, but don't work

--- webpack.config.js
+++ webpack.config.js
@@ -94,6 +94,10 @@ module.exports = {
                     from: path.resolve(__dirname, 'src/offline.html'),
                     to: path.resolve(__dirname, 'dist'),
                 },
+                {
+                    from: path.resolve(__dirname, 'src/manifest.json'),
+                    to: path.resolve(__dirname, 'dist'),
+                },
             ],
         }),
         new MiniCssExtractPlugin({
